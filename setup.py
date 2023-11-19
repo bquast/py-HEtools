6a3dc757d2c2aaa22c10e00182d0db8fc3e6d10f
Bastiaan Quast <bquast@gmail.com>
2023-11-19 08:48:19 -0600

    remove unneeded files

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
         name="HEtools", 
-        version="0.1.0",
+        version="0.3.0",
         author="Bastiaan Quast",
         author_email="<bquast@gmail.com>",
         packages=find_packages(),
         install_requires=['numpy',],
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         keywords=['homomorphic encryption', 'FHE', 'cryptography', 'coefmod', 'modulo', 'coefficient modulo', 'polynomial'],
         classifiers= [
             "Development Status :: 3 - Alpha",
